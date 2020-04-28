0aede7c3c2b94a199b26694b4104ad519b07ab5a
jmanuel2099 <jmanuel2099@gmail.com>
2020-04-27 22:18:32 -0500

    primer commit en pc nuevo

--- client/src/assets/js/initComponents.js
+++ client/src/assets/js/initComponents.js
@@ -2,3 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems);
+  });
